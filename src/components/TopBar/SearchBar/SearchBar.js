3bee1dd41855b8370865b34ba5b478f719a91bd1
jvhti <jvhti@hotmail.com>
2020-11-05 12:52:23 -0300

    Add label for SearchBar

--- src/components/TopBar/SearchBar/SearchBar.js
+++ src/components/TopBar/SearchBar/SearchBar.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Input from "../../UI/Input/Input";
 import classes from './SearchBar.module.scss';
 
-const searchBar = ({value, onChange}) => {
+const SearchBar = ({value, onChange}) => {
+  const inputRef = useRef(null);
+
   return (
       <div className={classes.SearchBarContainer}>
-        <Input placeholder="Buscar..." className={classes.SearchBar} value={value} onChange={onChange}/>
+        <label htmlFor={inputRef.current} className="sr-only">Pesquisar</label>
+        <Input ref={inputRef} placeholder="Buscar..." className={classes.SearchBar} value={value} onChange={onChange}/>
         <button className={classes.SearchButton}><span className="sr-only">Pesquisar</span></button>
       </div>
   );
 };
 
-export default React.memo(searchBar);
+export default React.memo(SearchBar);
