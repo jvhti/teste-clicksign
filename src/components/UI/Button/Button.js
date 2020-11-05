42408c69c79d701102d3318465c1ba9020d420a5
jvhti <jvhti@hotmail.com>
2020-11-05 12:11:12 -0300

    Implement disabled prop in the Button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import classes from "./Button.module.scss";
 
-const button = ({children, className, onClick}) => {
+const button = ({children, className, onClick, disabled = false}) => {
   const allClassName = classes.Button + (className ? " " + className : '');
 
-  return <button className={allClassName} onClick={onClick}>{children}</button>
+  return <button disabled={disabled} className={allClassName} onClick={onClick}>{children}</button>
 };
 
 export default React.memo(button);
