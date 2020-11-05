365d27c7dfbd43e680c644f5d1e17422ed0a0f5f
jvhti <jvhti@hotmail.com>
2020-11-05 11:50:08 -0300

    Highlight last created Contact

--- src/components/ContactTable/ContactItem.js
+++ src/components/ContactTable/ContactItem.js
@@ -5,9 +5,9 @@ import deleteIcon from "../../resources/img/ic-delete.svg";
 
 const colors = ['#fa8d68', '#90d26c', '#68a0fa', '#fab668', '#8368fa', '#fa68b5', '#5fe2c4', '#f55a5a'];
 
-const contactItem = ({name, email, number, deleteAction, editAction}) => {
+const contactItem = ({name, email, number, deleteAction, editAction, isHighlighted}) => {
   return (
-      <tr>
+      <tr className={isHighlighted ? classes.Active : undefined}>
         <td><span className={classes.ContactIcon}
                   style={{backgroundColor: colors[('Z'.charCodeAt(0) - name.trim().toUpperCase().charCodeAt(0)) % colors.length]}}>{name.trim().charAt(0)}</span>
         </td>
--- src/components/ContactTable/ContactTable.js
+++ src/components/ContactTable/ContactTable.js
@@ -1,8 +1,29 @@
 import React, {useCallback} from 'react';
 import classes from './ContactTable.module.scss';
 import ContactItem from "./ContactItem";
+import useForceUpdate from "../../hooks/forceUpdateHook";
+
+function isHighlighted(i, forceUpdate) {
+  if (window.lastSaved !== i || !window.timestamp) return false;
+
+  let t = new Date();
+  t.setSeconds(t.getSeconds() + 10);
+
+  if (window.timestamp < t) {
+    if (!window.timeout)
+      window.timeout = setTimeout(() => {
+        window.lastSaved = null;
+        window.timestamp = null;
+        window.timeout = null;
+        forceUpdate();
+      }, (t.getTime() - new Date().getTime()) + 5 * 100);
+    return true;
+  }
+  return false;
+}
 
 const ContactTable = ({contactList, setActiveModal, setContactId}) => {
+  const forceUpdate = useForceUpdate();
 
   const deleteAction = useCallback((i) => {
     return () => {
@@ -32,7 +53,8 @@ const ContactTable = ({contactList, setActiveModal, setContactId}) => {
         <tbody>
         {contactList.map((contact, i) => <ContactItem key={i} name={contact.name} email={contact.email}
                                                       number={contact.cellphone} deleteAction={deleteAction(i)}
-                                                      editAction={editAction(i)}/>)}
+                                                      editAction={editAction(i)}
+                                                      isHighlighted={isHighlighted(i, forceUpdate)}/>)}
         </tbody>
       </table>
   );
