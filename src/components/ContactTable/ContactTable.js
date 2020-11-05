e2ef43ced7f026261534437ff0f5b5145c265f15
jvhti <jvhti@hotmail.com>
2020-11-05 12:03:43 -0300

    Handle multiples highlighted contacts

--- src/components/ContactTable/ContactTable.js
+++ src/components/ContactTable/ContactTable.js
@@ -3,20 +3,22 @@ import classes from './ContactTable.module.scss';
 import ContactItem from "./ContactItem";
 import useForceUpdate from "../../hooks/forceUpdateHook";
 
+let indexHighlighted = [];
+
 function isHighlighted(i, forceUpdate) {
+  if (indexHighlighted.find(x => x === i)) return true;
   if (window.lastSaved !== i || !window.timestamp) return false;
 
-  let t = new Date();
+  let t = new Date(window.timestamp);
   t.setSeconds(t.getSeconds() + 10);
 
-  if (window.timestamp < t) {
-    if (!window.timeout)
-      window.timeout = setTimeout(() => {
-        window.lastSaved = null;
-        window.timestamp = null;
-        window.timeout = null;
-        forceUpdate();
-      }, (t.getTime() - new Date().getTime()) + 5 * 100);
+  if (t > new Date()) {
+    setTimeout(() => {
+      indexHighlighted = indexHighlighted.filter((ind) => ind !== i);
+      forceUpdate();
+    }, (t.getTime() - new Date().getTime()) + 5);
+
+    indexHighlighted.push(i);
     return true;
   }
   return false;
