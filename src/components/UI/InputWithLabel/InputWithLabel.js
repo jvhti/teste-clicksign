db931256c8640933388a924bfea04e8e54d87a84
jvhti <jvhti@hotmail.com>
2020-11-05 15:52:38 -0300

    Add type to InputWithLabel

--- src/components/UI/InputWithLabel/InputWithLabel.js
+++ src/components/UI/InputWithLabel/InputWithLabel.js
@@ -2,11 +2,12 @@ import React from 'react';
 import classes from './InputWithLabel.module.scss';
 import Input from "../Input/Input";
 
-const inputWithLabel = ({label, maxWidth, value, onChange}) => {
+const inputWithLabel = ({label, maxWidth, type, value, onChange}) => {
   return (
       <label className={classes.InputWithLabel}>
         {label}
-        <Input value={value} className={classes.InputWithLabel_Input} maxWidth={maxWidth} onChange={onChange}/>
+        <Input value={value} type={type} className={classes.InputWithLabel_Input} maxWidth={maxWidth}
+               onChange={onChange}/>
       </label>
   );
 };
